e24c430cb534396eae455b6c3742c395c3076e0c
marcos rossini <marcos.tengelmann@gmail.com>
2024-10-28 22:25:09 +0000

    player interface changed

--- life-back/src/game_management.py
+++ life-back/src/game_management.py
@@ -1,5 +1,5 @@
-import json
 from enum import Enum
+from typing import List
 
 class GameCondition(Enum):
     STARTED=1
@@ -7,9 +7,9 @@ class GameCondition(Enum):
     STOPPED=3
 
 class GameStatus:
-    def __init__(self, condition, players=0):
+    def __init__(self, condition,players=[]):
         self._condition : GameCondition=condition
-        self._players : int =players
+        self._players : List = players
     
     @property
     def players(self):
@@ -17,13 +17,10 @@ class GameStatus:
     
     @players.setter
     def players(self,value):
-        if not isinstance(value, int):
-            raise ValueError("Players must be an int")
+        if not isinstance(value, List):
+            raise ValueError("Players must be a list")
         
-        multiplier=1
-        if value < 0:
-            multiplier=-1
-        self._players = value * multiplier        
+        self._hlayers = value        
 
     @property
     def condition(self):
@@ -39,21 +36,25 @@ class GameStatus:
     def to_dict(self):
         return {
             'condition': self.condition.name if self.condition in GameCondition else 'UNKNOWN',
-            'players': self.players
+            'players': self._players
         }
 
 class Game:
     def __init__(self):
-        self.game_status=GameStatus(GameCondition.STOPPED,players=0)
+        self.game_status=GameStatus(GameCondition.STOPPED)
         self.round=0
         self.current_player=0
 
-    def start(self, players : int=16):
+    def start(self, players):
         self.game_status.condition=GameCondition.STARTED
-        self.game_status.players=players
+        self.game_status.players = self.__create_players(players.get('human',[]), players.get('computer',16))
         self.current_player=1
         self.round=1
         return self.status()
+    
+    def __create_players(self, humans, computer):
+        list_of_players = []
+        return list_of_players
     
     def pause(self):
         self.game_status.condition = GameCondition.PAUSED
